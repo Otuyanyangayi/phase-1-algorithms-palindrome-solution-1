d0ee1ee392d6a0c2b79ef523fb4bfce5268fd486
Grace <gracenyangayi@gmail.com>
2022-06-16 08:35:43 +0300

    commit tree db4e2c4291df4f9ae85ce1f5abd20438ea027185

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+function reverseString(word){
+  return word.split('').reverse().join('')
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word)
+  if (word === reversedWord) {
+     return true
+  } else {
+    return false 
+  }
+
 }
 
 /* 
   Add your pseudocode here
+  if the when the string is reversed is the same as input 
+  return true
+  else 
+  return false  
 */
 
 /*
   Add written explanation of your solution here
+  the code takes a string a reverses it. if the reversed string is the same as
+  the string input then it returns true
+  otherwise it returns false 
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +39,22 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("")
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+  
+  console.log("")
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+  
+  console.log("")
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+  
 }
 
 module.exports = isPalindrome;
